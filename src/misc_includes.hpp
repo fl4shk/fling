5c6e0d808034a92dd4054b4ea8ee8ce93e8a96dd
fl4shk <fl4shk@users.noreply.github.com>
2018-10-19 11:15:17 -0400

    Second commit

---
+++ src/misc_includes.hpp
@@ -0,0 +1,25 @@
+#ifndef src__slash__misc_includes_hpp
+#define src__slash__misc_includes_hpp
+
+
+// src/misc_includes.hpp
+
+
+#include "liborangepower_src/misc_includes.hpp"
+#include "liborangepower_src/misc_output_classes.hpp"
+#include "liborangepower_src/misc_types.hpp"
+#include "liborangepower_src/misc_utility_funcs.hpp"
+#include "liborangepower_src/misc_bitwise_funcs.hpp"
+#include "liborangepower_src/misc_defines.hpp"
+#include "liborangepower_src/gen_getter_setter_defines.hpp"
+#include "liborangepower_src/string_conversion_stuff.hpp"
+
+using namespace liborangepower::printout_etc;
+using namespace liborangepower::integer_types;
+using namespace liborangepower::misc_util;
+using namespace liborangepower::bitwise;
+using namespace liborangepower::strings;
+
+
+
+#endif		// src__slash__misc_includes_hpp
