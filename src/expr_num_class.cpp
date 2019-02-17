c7ff858609c2c324d84a1e384c22f6bae0cfe51d
fl4shk <fl4shk@users.noreply.github.com>
2019-02-17 13:43:48 -0600

    ExprNum done (?)

--- src/expr_num_class.cpp
+++ src/expr_num_class.cpp
@@ -3,43 +3,146 @@
 namespace frost_hdl
 {
 
+ExprNum::ExprNum(const RawExprNumData& s_data, bool s_is_signed)
+{
+	set_data(s_data);
+	set_is_signed(s_is_signed);
+}
+ExprNum::ExprNum(RawExprNumData&& s_data, bool s_is_signed)
+{
+	set_data(std::move(s_data));
+	set_is_signed(s_is_signed);
+}
+ExprNum::ExprNum(bool s_data, bool s_is_signed)
+{
+	set_size(1);
+	set_bit(0, s_data);
+	set_is_signed(s_is_signed);
+}
 
-//void ExprNum::copy_from_bignum(const BigNum& n_data, size_t n_data_size)
-//{
-//	BigNum mask = 1;
-//
-//	RawExprNumData temp_data;
-//
-//	for (size_t i=0; i<n_data_size; ++i)
-//	{
-//		//_data.push_back((n_data & mask) != BigNum(0));
-//		temp_data.push_back((n_data & mask) != BigNum(0));
-//		mask *= 2;
-//	}
-//
-//	set_data(temp_data);
-//}
-//
-//BigNum ExprNum::convert_to_bignum() const
-//{
-//	BigNum ret = 0;
-//
-//	for (size_t i=size(); i>0; --i)
-//	{
-//		ret *= 2;
-//		ret |= one_bit(i - 1);
-//	}
-//
-//	return ret;
-//}
+ExprNum::ExprNum(const BigNum& s_data, size_t s_data_size,
+	bool s_is_signed)
+{
+	set_is_signed(s_is_signed);
+	copy_from_bignum(s_data, s_data_size);
+}
 
-//void ExprNum::set_size(size_t n_size)
-//{
-//	//if (n_size > size())
-//	//{
-//	//	RawExprNumData 
-//	//}
-//}
+ExprNum::ExprNum(const ExprNum& src, size_t s_data_size)
+{
+	*this = src;
+	set_size(s_data_size);
+}
+ExprNum::ExprNum(const ExprNum& src, size_t s_data_size, bool s_is_signed)
+{
+	_data = src._data;
+	set_is_signed(s_is_signed);
 
+	// resizing needs to come *after* "_is_signed" is modified.
+	set_size(s_data_size);
+}
+
+void ExprNum::copy_from_bignum(const BigNum& n_data, size_t n_data_size)
+{
+	BigNum mask = 1;
+
+	RawExprNumData temp_data;
+
+	for (size_t i=0; i<n_data_size; ++i)
+	{
+		//_data.push_back((n_data & mask) != BigNum(0));
+		temp_data.push_back((n_data & mask) != BigNum(0));
+		mask *= 2;
+	}
+
+	set_data(temp_data);
+}
+
+BigNum ExprNum::convert_to_bignum() const
+{
+	BigNum ret = 0;
+
+	for (size_t i=size(); i>0; --i)
+	{
+		ret *= 2;
+
+		if (is_negative())
+		{
+			ret |= !one_bit(i - 1);
+		}
+		else // if (!is_negative())
+		{
+			ret |= one_bit(i - 1);
+		}
+	}
+
+	if (is_negative())
+	{
+		ret += 1;
+		ret = -ret;
+	}
+
+	return ret;
+}
+
+void ExprNum::set_size(size_t n_size)
+{
+	RawExprNumData n_data(data());
+	n_data.resize(n_size);
+
+	if (n_size > size())
+	{
+		if (!is_signed())
+		{
+			for (size_t i=size(); i<n_data.size(); ++i)
+			{
+				n_data.at(i) = 0;
+			}
+		}
+		else // if (is_signed())
+		{
+			if (size() > 0)
+			{
+				for (size_t i=size(); i<n_data.size(); ++i)
+				{
+					n_data.at(i) = one_bit(size() - 1);
+				}
+			}
+			else // if (size() == 0)
+			{
+				for (size_t i=size(); i<n_data.size(); ++i)
+				{
+					n_data.at(i) = 0;
+				}
+			}
+		}
+	}
+
+	set_data(n_data);
+}
+
+RawExprNumData ExprNum::multiple_bits(size_t start_index, size_t amount)
+	const
+{
+	RawExprNumData ret;
+
+	for (size_t i=start_index; i<(start_index + amount); ++i)
+	{
+		ret.push_back(one_bit(i));
+	}
+
+	return ret;
+}
+void ExprNum::set_multiple_bits(size_t start_index,
+	const RawExprNumData& some_bits)
+{
+	auto n_data(data());
+
+	for (size_t i=0; i<some_bits.size(); ++i)
+	{
+		n_data.at(start_index + i) = some_bits.at(i);
+	}
+
+	set_data(n_data);
+}
 
 } // namespace frost_hdl
--- src/expr_num_class.hpp
+++ src/expr_num_class.hpp
@@ -20,32 +20,51 @@ public:		// functions
 
 	ExprNum(const RawExprNumData& s_data, bool s_is_signed=false);
 	ExprNum(RawExprNumData&& s_data, bool s_is_signed=false);
-	ExprNum(bool s_data, bool s_is_signed=false);
+	explicit ExprNum(bool s_data, bool s_is_signed=false);
 
 
-	ExprNum(const BigNum& s_data, size_t s_data_size,
+	explicit ExprNum(const BigNum& s_data, size_t s_data_size,
 		bool s_is_signed=false);
 
+	ExprNum(const ExprNum& src, size_t s_data_size);
+	ExprNum(const ExprNum& src, size_t s_data_size, bool s_is_signed);
 
 
 	ExprNum(const ExprNum& to_copy) = default;
-	ExprNum(ExprNum&& to_move) = default;
 
 
 	virtual ~ExprNum() = default;
 
 
 	ExprNum& operator = (const ExprNum& to_copy) = default;
-	ExprNum& operator = (ExprNum&& to_move) = default;
+
+
 
 	void copy_from_bignum(const BigNum& n_data, size_t n_data_size);
+	inline void copy_from_bignum(const BigNum& n_data, size_t n_data_size,
+		bool n_is_signed)
+	{
+		copy_from_bignum(n_data, n_data_size);
+		set_is_signed(n_is_signed);
+	}
 
 	BigNum convert_to_bignum() const;
+	inline std::string convert_to_string() const
+	{
+		return liborangepower::bignum::convert_bignum_to_str
+			(convert_to_bignum());
+	}
 
-	inline operator BigNum () const
+	inline operator BigNum() const
 	{
 		return convert_to_bignum();
 	}
+
+	explicit inline operator std::string() const
+	{
+		return convert_to_string();
+	}
+
 
 
 	inline const auto& data() const
@@ -63,13 +82,16 @@ public:		// functions
 	{
 		return data().at(index);
 	}
+	RawExprNumData multiple_bits(size_t start_index, size_t amount) const;
 
 	inline void set_bit(size_t index, bool val)
 	{
-		RawExprNumData n_data(data());
+		auto n_data(data());
 		n_data.at(index) = val;
 		set_data(n_data);
 	}
+	void set_multiple_bits(size_t start_index,
+		const RawExprNumData& some_bits);
 
 
 	inline void set_data(const RawExprNumData& n_data)
@@ -79,6 +101,11 @@ public:		// functions
 	inline void set_data(RawExprNumData&& n_data)
 	{
 		_data = dup_expr_num_data(std::move(n_data));
+	}
+
+	inline bool is_negative() const
+	{
+		return (is_signed() && one_bit(size() - 1));
 	}
 
 	GEN_GETTER_AND_SETTER_BY_VAL(is_signed)
