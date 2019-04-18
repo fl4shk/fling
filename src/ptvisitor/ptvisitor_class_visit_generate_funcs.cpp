6870fa877b470a5f59b10516e391a5c7f2ff7ad4
fl4shk <fl4shk@users.noreply.github.com>
2019-04-18 08:34:48 -0500

    switch back to std::variant

--- src/ptvisitor/ptvisitor_class_visit_generate_funcs.cpp
+++ src/ptvisitor/ptvisitor_class_visit_generate_funcs.cpp
@@ -40,23 +40,23 @@ auto PTVisitor::visitGenerateBlockInModule
 		with(top_level_context, with_stacks_small_num(this, SmallNum
 			(ModuleOrInterface::Module)))
 		{
-			//if (std::holds_alternative<FrostModule*>(module_scope
-			//	->parent()))
-			if (!module_scope->parent().holds_generate_block_in_module)
+			if (std::holds_alternative<FrostModule*>(module_scope
+				->parent()))
+			//if (!module_scope->parent().holds_generate_block_in_module)
 			{
-				//curr_generate_block->set_parent(std::get
-				//	<FrostModule*>(module_scope->parent()));
-				curr_generate_block->set_parent(module_scope->parent()
-					.frost_module);
+				curr_generate_block->set_parent(std::get
+					<FrostModule*>(module_scope->parent()));
+				//curr_generate_block->set_parent(module_scope->parent()
+				//	.frost_module);
 			}
-			//else if (std::holds_alternative<FrostGenerateBlockInModule*>
-			//	(module_scope->parent()))
-			else if (module_scope->parent().holds_generate_block_in_module)
+			else if (std::holds_alternative<FrostGenerateBlockInModule*>
+				(module_scope->parent()))
+			//else if (module_scope->parent().holds_generate_block_in_module)
 			{
-				//curr_generate_block->set_parent(std::get
-				//	<FrostGenerateBlockInModule*>(module_scope->parent()));
-				curr_generate_block->set_parent(module_scope->parent()
-					.frost_generate_block_in_module);
+				curr_generate_block->set_parent(std::get
+					<FrostGenerateBlockInModule*>(module_scope->parent()));
+				//curr_generate_block->set_parent(module_scope->parent()
+				//	.frost_generate_block_in_module);
 			}
 			else
 			{
