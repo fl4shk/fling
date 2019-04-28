f1c96990df5bffc19cd51f03fc182c16f0508881
fl4shk <fl4shk@users.noreply.github.com>
2019-04-28 17:05:36 -0500

    stuffs

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,27 @@
 
 int main(int argc, char** argv)
 {
-	// We need at least *one* source file.
-	if (argc < 2)
-	{
-		printerr("Usage:  ", argv[0], " <source_files>\n");
-		exit(1);
-	}
+	CircLinkedList<int> a;
+	a.insert_after(*(&a.begin()), 3);
 
-	frost_hdl::PTVisitor::ListParsedSrcCode s_list_parsed_src_code;
+	//// We need at least *one* source file.
+	//if (argc < 2)
+	//{
+	//	printerr("Usage:  ", argv[0], " <source_files>\n");
+	//	exit(1);
+	//}
 
-	//printout("argc:  ", argc, "\n");
+	//frost_hdl::PTVisitor::ListParsedSrcCode s_list_parsed_src_code;
 
-	for (int i=1; i<argc; ++i)
-	{
-		s_list_parsed_src_code.push_back(std::unique_ptr<frost_hdl
-			::ParsedSrcCode>(new frost_hdl::ParsedSrcCode(argv[i])));
-	}
+	////printout("argc:  ", argc, "\n");
 
-	frost_hdl::PTVisitor visitor(std::move(s_list_parsed_src_code));
-	return visitor.run();
+	//for (int i=1; i<argc; ++i)
+	//{
+	//	s_list_parsed_src_code.push_back(std::unique_ptr<frost_hdl
+	//		::ParsedSrcCode>(new frost_hdl::ParsedSrcCode(argv[i])));
+	//}
+
+	//frost_hdl::PTVisitor visitor(std::move(s_list_parsed_src_code));
+	//return visitor.run();
 
 }
