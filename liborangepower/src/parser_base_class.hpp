b456379a4f5b8ba6081bc9f1bbd0aea07f58b1c6
fl4shk <fl4shk@users.noreply.github.com>
2019-06-29 11:29:25 -0500

    git subrepo pull liborangepower
    subrepo:
      subdir:   "liborangepower"
      merged:   "68a0f49"
    upstream:
      origin:   "https://github.com/fl4shk/liborangepower.git"
      branch:   "master"
      commit:   "68a0f49"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- liborangepower/src/parser_base_class.hpp
+++ liborangepower/src/parser_base_class.hpp
@@ -184,7 +184,7 @@ protected:		// functions
 		{
 			for (size_t i=0; i<lss().found_set.size(); ++i)
 			{
-				_lexer()._next_tok();
+				_lexer().next_tok(just_test());
 			}
 		}
 		else // if (!lss().valid())
@@ -260,7 +260,8 @@ protected:		// functions
 	void _expect(TokType tok, const TokToStringMap& some_tok_ident_map,
 		const LexerState* lex_state=nullptr)
 	{
-		if (tok)
+		if ((lex_state != nullptr) ? (_lexer.state().tok() == tok)
+			: (lex_state->tok() == tok))
 		{
 			_err(_msg_for_expect(tok, some_tok_ident_map, lex_state));
 		}
