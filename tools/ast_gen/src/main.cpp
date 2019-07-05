280e52d2d5ec7943bf6dc19c5223986a6a747501
fl4shk <fl4shk@users.noreply.github.com>
2019-07-05 16:07:54 -0500

    stuffs

--- tools/ast_gen/src/main.cpp
+++ tools/ast_gen/src/main.cpp
@@ -1,6 +1,15 @@
 #include "misc_includes.hpp"
+#include "ast_gen_class.hpp"
 
 int main(int argc, char** argv)
 {
+	if (argc != 2)
+	{
+		printerr("Usage:  ", argv[0], " filename\n");
+		exit(1);
+	}
+	AstGen(std::vector({string(argv[0])})).run();
+
+
 	return 0;
 }
